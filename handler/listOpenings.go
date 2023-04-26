45531c9211f795b45c881b70270dbd4fd408bd69
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-04-26 17:14:05 -0300

    📝 docs: add documentation for list Openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1/
+// @Summary List all openings
+// @Description List all job openings
+// @Tags Openings
+// @Produce  json
+// @Success 200 {object} OpeningsResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 	if err := db.Find(&openings).Error; err != nil {
