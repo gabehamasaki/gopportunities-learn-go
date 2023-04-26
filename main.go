ba325da28927fa282997ebe3a4e3ba961cb8ba71
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-04-26 13:01:23 -0300

    ✨ feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/gabehamasaki/gopportunities-learn-go/router"
+import (
+	"github.com/gabehamasaki/gopportunities-learn-go/config"
+	"github.com/gabehamasaki/gopportunities-learn-go/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v",err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
