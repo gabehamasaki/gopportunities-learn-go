649660e36a36900ed0aeef56157b95daa8927503
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-04-26 11:52:09 -0300

    feat: add routes logic

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import "github.com/gabehamasaki/gopportunities-learn-go/router"
 
-
-
 func main() {
-
 	// Initialize Router
 	router.Initialize()
 }
--- router/router.go
+++ router/router.go
@@ -3,13 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize the router
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-      "message": "pong!",
-    })
-	})
+	// Initialize routes
+	initializeRoutes(router)
 
+	// Start server
 	router.Run(":8080")
 }
